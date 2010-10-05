78f61d2cea6aefd4dc4602014b9ea265e5cbc313
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2010-10-05 14:27:26 +0000

    make the tests pass again, by removing unneeded package installs in the test setup

--- src/z3c/recipe/i18n/tests.py
+++ src/z3c/recipe/i18n/tests.py
@@ -21,7 +21,6 @@ import zc.buildout.testing
 
 def setUp(test):
     zc.buildout.testing.buildoutSetUp(test)
-    zc.buildout.testing.install('RestrictedPython', test)
     zc.buildout.testing.install('ZConfig', test)
     zc.buildout.testing.install('ZODB3', test)
     zc.buildout.testing.install('pytz', test)
@@ -34,40 +33,26 @@ def setUp(test):
     zc.buildout.testing.install('zope.app.applicationcontrol', test)
     zc.buildout.testing.install('zope.applicationcontrol', test)
     zc.buildout.testing.install('zope.app.appsetup', test)
-    zc.buildout.testing.install('zope.app.component', test)
-    zc.buildout.testing.install('zope.app.container', test)
     zc.buildout.testing.install('zope.app.locales', test)
-    zc.buildout.testing.install('zope.app.pagetemplate', test)
     zc.buildout.testing.install('zope.app.publication', test)
     zc.buildout.testing.install('zope.authentication', test)
     zc.buildout.testing.install('zope.broken', test)
     zc.buildout.testing.install('zope.browser', test)
-    zc.buildout.testing.install('zope.cachedescriptors', test)
     zc.buildout.testing.install('zope.component', test)
-    zc.buildout.testing.install('zope.componentvocabulary', test)
     zc.buildout.testing.install('zope.configuration', test)
     zc.buildout.testing.install('zope.container', test)
     zc.buildout.testing.install('zope.contenttype', test)
-    zc.buildout.testing.install('zope.copy', test)
-    zc.buildout.testing.install('zope.copypastemove', test)
-    zc.buildout.testing.install('zope.datetime', test)
-    zc.buildout.testing.install('zope.deferredimport', test)
-    zc.buildout.testing.install('zope.deprecation', test)
     zc.buildout.testing.install('zope.dottedname', test)
-    zc.buildout.testing.install('zope.dublincore', test)
     zc.buildout.testing.install('zope.error', test)
     zc.buildout.testing.install('zope.event', test)
     zc.buildout.testing.install('zope.exceptions', test)
     zc.buildout.testing.install('zope.filerepresentation', test)
-    zc.buildout.testing.install('zope.formlib', test)
-    zc.buildout.testing.install('zope.hookable', test)
     zc.buildout.testing.install('zope.i18n', test)
     zc.buildout.testing.install('zope.i18nmessageid', test)
     zc.buildout.testing.install('zope.interface', test)
     zc.buildout.testing.install('zope.lifecycleevent', test)
     zc.buildout.testing.install('zope.location', test)
     zc.buildout.testing.install('zope.minmax', test)
-    zc.buildout.testing.install('zope.pagetemplate', test)
     zc.buildout.testing.install('zope.processlifetime', test)
     zc.buildout.testing.install('zope.proxy', test)
     zc.buildout.testing.install('zope.publisher', test)
@@ -77,11 +62,8 @@ def setUp(test):
     zc.buildout.testing.install('zope.site', test)
     zc.buildout.testing.install('zope.size', test)
     zc.buildout.testing.install('zope.tal', test)
-    zc.buildout.testing.install('zope.tales', test)
-    zc.buildout.testing.install('zope.testing', test)
     zc.buildout.testing.install('zope.traversing', test)
     zc.buildout.testing.install_develop('z3c.recipe.i18n', test)
-
 
 checker = renormalizing.RENormalizing([
     zc.buildout.testing.normalize_path,
@@ -104,7 +86,8 @@ checker = renormalizing.RENormalizing([
 def test_suite():
     return unittest.TestSuite(
         doctest.DocFileSuite('README.txt',
-            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            setUp=setUp,
+            tearDown=zc.buildout.testing.buildoutTearDown,
             optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
             checker=checker),
         )
